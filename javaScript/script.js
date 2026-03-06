98a464fc251b8f1a67f03d2a091b7b63a1f74d4a
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-06 23:55:36 +0530

    all filter button toggle complete

--- javaScript/script.js
+++ javaScript/script.js
@@ -4,3 +4,27 @@ const allIssuesContainer = document.getElementById("issue-all-card");
 let allIssueBtn = document.getElementById("all-btn");
 let openIssueBtn = document.getElementById("open-btn");
 let closedIssueBtn = document.getElementById("close-btn");
+
+
+
+
+
+
+
+
+
+
+
+// all filter btn
+function showFilterIssueBtn(id) {
+
+    allIssueBtn.classList.remove("bg-[#422ad5]", "text-white")
+    openIssueBtn.classList.remove("bg-[#422ad5]", "text-white")
+    closedIssueBtn.classList.remove("bg-[#422ad5]", "text-white")
+
+    let btn = document.getElementById(id);
+    btn.classList.add("bg-[#422ad5]", "text-white");
+    console.log(btn)
+
+}
+
