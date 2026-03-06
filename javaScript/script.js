3d83d31e78d042e40c284de80d1f9722212c1c19
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-06 23:51:54 +0530

    all card container access and all filter btn access

---
+++ javaScript/script.js
@@ -0,0 +1,6 @@
+
+const allIssuesContainer = document.getElementById("issue-all-card");
+
+let allIssueBtn = document.getElementById("all-btn");
+let openIssueBtn = document.getElementById("open-btn");
+let closedIssueBtn = document.getElementById("close-btn");
